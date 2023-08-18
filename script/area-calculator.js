2df90c00e89cd9c5872e78734381fa3ee9ec7aa1
joykbiswas <joykumarbiswas100@gmail.com>
2023-08-18 12:48:40 +0600

    rhombus area calculate

--- script/area-calculator.js
+++ script/area-calculator.js
@@ -16,17 +16,61 @@ function calculateTriangleArea(){
 
 }
 
-function calculateRectangleArea(){
-    const widthField = document.getElementById('rectangle-width');
-    const widthValueText = widthField.value;
-    const width = parseFloat(widthValueText);
+//reduce duplicate
 
-    const lengthField = document.getElementById('rectangle-length');
-    const lengthValueText = lengthField.value;
-    const length = parseFloat(lengthValueText);
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText)
+    return value;
+}
 
-    const area = width * length;
+function setElementInnerText(fieldId,area){
+    const element = document.getElementById(fieldId);
+    element.innerText = area
+}
+// Rectangle calculate Area
+function  calculateRectangleArea() {
+    const width = getInputValue('rectangle-width');
+    const length = getInputValue('rectangle-length');
 
-    const rectangleAreaSpan = document.getElementById('rectangle-area')
-    rectangleAreaSpan.innerText = area;
+    area = width * length;
+
+    setElementInnerText('rectangle-area', area)
+}
+ 
+//Parallelogram calculate area
+function calculateParallelogramArea() {
+   const base = getInputValue('parallelogram-base');
+   const height = getInputValue('parallelogram-height');
+
+   area = base * height;
+   setElementInnerText('parallelogram-area', area)
+}
+
+//ellipse calculation area
+
+function calculateEllipseArea(){
+    const large = getInputValue('ellipse-large')
+    const short = getInputValue('ellipse-short');
+
+    area = 3.14 * large * short
+
+    setElementInnerText('ellipse-area', area);
+}
+
+//pentagon calculation area
+function calculatePentagonArea(){
+    const side = getInputValue('pentagon-side');
+    area = 1.72 * side * side;
+    setElementInnerText('pentagon-area', area)
+}
+
+//Rhombus calculate area
+function calculateRhombusArea() {
+    const short = getInputValue('diagonal-short');
+    const large = getInputValue('diagonal-large');
+
+    area = 0.5 * short * large
+    setElementInnerText('rhombus-area', area)
 }
